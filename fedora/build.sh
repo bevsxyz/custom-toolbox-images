6f6a44e7720af016150a3f8d1259e525e31bc05e
Bevan Stanely <60595515+bevsxyz@users.noreply.github.com>
2023-09-19 16:43:18 +0530

    fix: build error

--- fedora/build.sh
+++ fedora/build.sh
@@ -1,2 +1,4 @@
 podman build -t fedora-personal .
-toolbox create --image localhost/fedora-personal
+FEDORA_IM=fedora-toolbox-$(cat /etc/fedora-release | tr -d -c 0-9)
+toolbox rm $FEDORA_IM
+toolbox create --image localhost/fedora-personal:latest $FEDORA_IM
